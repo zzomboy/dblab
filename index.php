f3fd47cc6e4288740b8497fe4163bd9035fe08eb
karanpoj <karanpoj@gmail.com>
2017-09-23 03:30:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>IT Online Shopping website</title>
+		<link href="css/reset.css" rel="stylesheet" type="text/css">
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+<!--header-->
+		<div class="header">
+			<!--Logo-->
+			<div class="logo">
+				<a href="index.php"><img src="img/DLZ.png" width="200px" height="92px"></a>
+			</div>
+			<!--right nav-->
+			<div class="header_nav">
+				<ul>
+					<li><a href="#">Register</a></li>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">My Account</a></li>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+
+		<!--menu bar-->
+		<div class="menu_bar">
+	     	<div class="menu">
+	     		<ul>
+			    	<li class="active"><a href="index.php">Home</a></li>
+			    	<li><a href="#">Product</a></li>
+			    	<li><a href="#">Pricelist</a></li>
+			    	<li><a href="#">About</a></li>
+			    	<li><a href="#">Contact</a></li>
+			    	<div class="clear"></div>
+     			</ul>
+	     	</div>
+	     	<div class="search_box">
+	     		<form>
+	     			<input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}"><input type="submit" value="">
+	     		</form>
+	     	</div>
+	     	<div class="clear"></div>
+	    </div>	 
+
+<!--Content-->
+	    <!--category & picture-->
+	    <div class="category_picture">
+			<div class="category_picture_left">				
+				<div class="categories">
+				  <ul>
+				  	<h3>Categories</h3>
+				      <li><a href="#">Mobile Phones</a></li>
+				      <li><a href="#">Desktop</a></li>
+				      <li><a href="#">Laptop</a></li>
+				      <li><a href="#">Accessories</a></li>
+				      <li><a href="#">Software</a></li>
+				       <li><a href="#">Sports &amp; Fitness</a></li>
+				       <li><a href="#">Footwear</a></li>
+				       <li><a href="#">Jewellery</a></li>
+				       <li><a href="#">Clothing</a></li>
+				       <li><a href="#">Home Decor &amp; Kitchen</a></li>
+				       <li><a href="#">Beauty &amp; Healthcare</a></li>
+				       <li><a href="#">Toys, Kids &amp; Babies</a></li>
+				  </ul>
+				</div>					
+	  	     </div>
+	    	<div class="clear"></div>
+		</div>
+
+<!--footer-->
+		<div class="footer">
+			<table class="section">
+				<tr>
+					<td>
+						<h4>DLZ IT Store</h4>
+						<ul>
+							<li><a href="#">Product</a></li>
+			    			<li><a href="#">Pricelist</a></li>
+			    			<li><a href="#">About</a></li>
+			    			<li><a href="#">Contact</a></li>
+						</ul>
+					</td>
+					<td>
+						<h4>My account</h4>
+						<ul>
+							<li><a href="#">Sign In</a></li>
+							<li><a href="#">My Profile</a></li>
+							<li><a href="#">Help</a></li>
+						</ul>
+					</td>
+					<td>
+						<h4>Contact</h4>
+						<ul>
+							<li><span>+66-9999-99999</span></li>
+							<li><span>+66-8888-88888</span></li>
+						</ul>
+					</td>
+					<td>
+						<h4>Shipping</h4>
+						<a href="http://www.dhl.co.th/th.html" target="_blank">
+							<img align="left" src="https://www.jib.co.th/web/images/footer/tran-dhl.png">
+						</a>
+						<a href="https://th.kerryexpress.com/th/track/" target="_blank">
+							<img align="left" src="https://www.jib.co.th/web/images/footer/tran-kerry.png">
+						</a>
+						<a href="https://www.tnt.com/express/th_th/site/home.html" target="_blank">
+							<img align="left" src="https://www.jib.co.th/web/images/footer/tran-tnt.png">
+						</a>
+						<br><br><br>
+						<h4>payment</h4>	
+						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-SCB.jpg">
+						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-SB.jpg">
+						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-KB.jpg">
+						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-KTB.jpg">
+						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-TBB.jpg">
+						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-crdito.jpg">
+					</td>
+				</tr>
+			</table>		
+       		<div class="copy_right">
+				<p>DLZ © All rights Reseverd | Design by  <a href="http://www.facebook.com/karanpoj">Karanpoj</a> </p>
+			</div>
+    	</div>
+	</body>
+</html>
