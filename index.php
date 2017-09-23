6fa47f9f024fcefdb74f3cde8847178ce446fa04
karanpoj <karanpoj@gmail.com>
2017-09-23 18:54:50 +0700

    complete template

--- index.php
+++ index.php
@@ -9,30 +9,31 @@
 	<body>
 <!--header-->
 		<div class="header">
-			<!--Logo-->
+<!--Logo-->
 			<div class="logo">
 				<a href="index.php"><img src="img/DLZ.png" width="200px" height="92px"></a>
 			</div>
-			<!--right nav-->
+<!--right nav-->
 			<div class="header_nav">
 				<ul>
-					<li><a href="#">Register</a></li>
-					<li><a href="#">Login</a></li>
-					<li><a href="#">My Account</a></li>
+					<li><a href="register.php">Register</a></li>
+					<li><a href="login.php">Login</a></li>
+					<li><a href="my_account.php">My Account</a></li>
 				</ul>
 			</div>
 			<div class="clear"></div>
 		</div>
 
-		<!--menu bar-->
+<!--menu bar-->
 		<div class="menu_bar">
 	     	<div class="menu">
 	     		<ul>
 			    	<li class="active"><a href="index.php">Home</a></li>
-			    	<li><a href="#">Product</a></li>
-			    	<li><a href="#">Pricelist</a></li>
-			    	<li><a href="#">About</a></li>
-			    	<li><a href="#">Contact</a></li>
+			    	<li><a href="promotion.php">Promotion</a></li>
+			    	<li><a href="product.php">Product</a></li>
+			    	<li><a href="pricelist.php">Pricelist</a></li>
+			    	<li><a href="about.php">About</a></li>
+			    	<li><a href="contact.php">Contact</a></li>
 			    	<div class="clear"></div>
      			</ul>
 	     	</div>
@@ -45,21 +46,141 @@
 	    </div>	 
 
 <!--Content-->
-	    <!--category & picture-->
+<!--category & picture-->
 	    <div class="category_picture">			
 			<div class="categories">
 				<ul>
 					<h3>Categories</h3>
-					<li><a href="#">CPU</a></li>
-				    <li><a href="#">Mainboard</a></li>
-				    <li><a href="#">Graphic card</a></li>
-				    <li><a href="#">Harddisk & Solid state drive</a></li>
-				    <li><a href="#">RAM for PC</a></li>
-				    <li><a href="#">Case & Power supply</a></li>
-				    <li><a href="#">Optical disk drive</a></li>
+					<li><a href="product.php">Laptop</a></li>
+					<li><a href="product.php">CPU</a></li>
+				    <li><a href="product.php">Mainboard</a></li>
+				    <li><a href="product.php">Graphic card</a></li>
+				    <li><a href="product.php">Monitor</a></li>
+				    <li><a href="product.php">Harddisk & Solid state drive</a></li>
+				    <li><a href="product.php">RAM for PC</a></li>
+				    <li><a href="product.php">Case & Power supply</a></li>
+				    <li><a href="product.php">Optical disk drive</a></li>
+				    <li><a href="product.php">Speaker & Sound card</a></li>
 				</ul>
 			</div>					
-	    	<div class="clear"></div>
+	    	
+			<div class="index_right_content">
+				<div class="promo_overview">
+					<div class="promotion_bar">
+						<div class="heading">
+    						<h3>Best sale</h3>
+    					</div>
+    					<div class="see_all">
+    						<p><a href="promotion.php">See all Promotions</a></p>
+    					</div>
+    					<div class="clear"></div>
+					</div>
+					<div class="tb_promo">
+						<div class="product_box">
+							<img align="left" src="img/monitor.png">
+							<h2>Monitor</h2>
+							<div class="price_details">
+								<p>฿ 5999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/hdd.png">
+							<h2>Harddisk</h2>
+							<div class="price_details">
+								<p>฿ 2999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/cpu.png">
+							<h2>CPU</h2>
+							<div class="price_details">
+								<p>฿ 9999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/speaker.jpg">
+							<h2>Speaker</h2>
+							<div class="price_details">
+								<p>฿ 999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+
+						<div class="product_box">
+							<img align="left" src="img/monitor.png">
+							<h2>Monitor</h2>
+							<div class="price_details">
+								<p>฿ 5999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/hdd.png">
+							<h2>Harddisk</h2>
+							<div class="price_details">
+								<p>฿ 2999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/cpu.png">
+							<h2>CPU</h2>
+							<div class="price_details">
+								<p>฿ 9999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/speaker.jpg">
+							<h2>Speaker</h2>
+							<div class="price_details">
+								<p>฿ 999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/speaker.jpg">
+							<h2>Speaker</h2>
+							<div class="price_details">
+								<p>฿ 999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+
+						<div class="product_box">
+							<img align="left" src="img/monitor.png">
+							<h2>Monitor</h2>
+							<div class="price_details">
+								<p>฿ 5999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/hdd.png">
+							<h2>Harddisk</h2>
+							<div class="price_details">
+								<p>฿ 2999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+						<div class="product_box">
+							<img align="left" src="img/cpu.png">
+							<h2>CPU</h2>
+							<div class="price_details">
+								<p>฿ 9999</p>
+								<div class="clear"></div>
+							</div>
+						</div>
+
+					</div>
+    				<div class="clear"></div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="clear"></div>
 		</div>
 
 <!--footer-->
@@ -69,18 +190,20 @@
 					<td>
 						<h4>DLZ IT Store</h4>
 						<ul>
-							<li><a href="#">Product</a></li>
-			    			<li><a href="#">Pricelist</a></li>
-			    			<li><a href="#">About</a></li>
-			    			<li><a href="#">Contact</a></li>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="promotion.php">Promotion</a></li>
+							<li><a href="product.php">Product</a></li>
+			    			<li><a href="pricelist.php">Pricelist</a></li>
+			    			<li><a href="about.php">About</a></li>
+			    			<li><a href="contact.php">Contact</a></li>
 						</ul>
 					</td>
 					<td>
 						<h4>My account</h4>
 						<ul>
-							<li><a href="#">Sign In</a></li>
-							<li><a href="#">My Profile</a></li>
-							<li><a href="#">Help</a></li>
+							<li><a href="login">Login</a></li>
+							<li><a href="my_account.php">My Profile</a></li>
+							<li><a href="help.php">Help</a></li>
 						</ul>
 					</td>
 					<td>
@@ -88,27 +211,27 @@
 						<ul>
 							<li><span>+66-9999-99999</span></li>
 							<li><span>+66-8888-88888</span></li>
+							<br><br>
+							<h4>Shipping</h4>
+							<a href="http://www.dhl.co.th/th.html" target="_blank">
+								<img align="left" src="img/tran-dhl.png">
+							</a>
+							<a href="https://th.kerryexpress.com/th/track/" target="_blank">
+								<img align="left" src="img/tran-kerry.png">
+							</a>
+							<a href="https://www.tnt.com/express/th_th/site/home.html" target="_blank">
+								<img align="left" src="img/tran-tnt.png">
+							</a>
 						</ul>
 					</td>
 					<td>
-						<h4>Shipping</h4>
-						<a href="http://www.dhl.co.th/th.html" target="_blank">
-							<img align="left" src="https://www.jib.co.th/web/images/footer/tran-dhl.png">
-						</a>
-						<a href="https://th.kerryexpress.com/th/track/" target="_blank">
-							<img align="left" src="https://www.jib.co.th/web/images/footer/tran-kerry.png">
-						</a>
-						<a href="https://www.tnt.com/express/th_th/site/home.html" target="_blank">
-							<img align="left" src="https://www.jib.co.th/web/images/footer/tran-tnt.png">
-						</a>
-						<br><br><br>
 						<h4>payment</h4>	
-						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-SCB.jpg">
-						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-SB.jpg">
-						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-KB.jpg">
-						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-KTB.jpg">
-						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-TBB.jpg">
-						<img align="left" src="https://www.jib.co.th/web/images/footer/bank-crdito.jpg">
+						<img align="left" src="img/bank-SCB.jpg">
+						<img align="left" src="img/bank-SB.jpg">
+						<img align="left" src="img/bank-KB.jpg">
+						<img align="left" src="img/bank-KTB.jpg">
+						<img align="left" src="img/bank-TBB.jpg">
+						<img align="left" src="img/bank-crdito.jpg">
 					</td>
 				</tr>
 			</table>		
