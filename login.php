8be901d0a6f7829ea15f69ed0df8ba08e39338ff
karanpoj <karanpoj@gmail.com>
2017-09-25 03:49:21 +0700

    add login page
    del alert msg

--- login.php
+++ login.php
@@ -46,114 +46,20 @@
 	    </div>	 
 
 <!--Content-->
-<!--register form-->
-		<div class="register_form">
-			<h2>Register</h2>
+<!--login form-->
+		<div class="register_form login_form">
+			<h2>Login</h2>
 			<form method="post" action="index.php">
-  				<table class="form_box">
-
-  					<tr>
-  						<td>
-  							Name :
-    					</td>
-    					<td>
-    						<input type="text" placeholder="Enter Name" name="uname" required>
-    					</td>
-    					<td><!--Please enter your e-mail--></td>
-    				</tr>
-
-    				<tr>
-  						<td>
-  							Surname :
-    					</td>
-    					<td>
-    						<input type="text" placeholder="Enter Surname" name="sname" required>
-    					</td>
-    					<td><!--Please enter your e-mail--></td>
-    				</tr>
-
-    				<tr>
-  						<td>
-  							Mobile phone :
-    					</td>
-    					<td>
-    						<input type="text" placeholder="Enter Mobile phone" name="mobile" required>
-    					</td>
-    					<td><!--Please enter your e-mail--></td>
-    				</tr>
-
-  					<tr>
-  						<td>
-  							Email :
-    					</td>
-    					<td>
-    						<input type="text" placeholder="Enter Email" name="email" required>
-    					</td>
-    					<td><!--Please enter your e-mail--></td>
-    				</tr>
-
-    				<tr>
-  						<td>
-    					Password :
-    					
-    					</td>
-    					<td>
-    						<input type="password" placeholder="Enter Password" name="psw" required>
-    					</td>
-    					<td></td>
-    				</tr>
-
-    				<tr>
-  						<td>
-    						Repeat Password :
-    					</td>
-    					<td>
-    						<input type="password" placeholder="Re-enter Password" name="psw-repeat" required>
-    					</td>
-    					<td></td>
-    				</tr>
-
-    				<tr>
-  						<td>
-  							Gender :
-    					</td>
-    					<td style="padding: 15px 0;">
-    						<input type="radio" name="gender" value="male" required>Male
-    						<input type="radio" name="gender" value="female" required>Female
-    					</td>
-    					<td><!--Please enter your e-mail--></td>
-    				</tr>
-
-    				<tr>
-  						<td>
-  							Birthday :
-    					</td>
-    					<td>
-    						<input class="input_date" type="date" name="birthday" required>
-    					</td>
-    					<td><!--Please enter your e-mail--></td>
-    				</tr>
-
-    				<tr>
-    					<td></td>
-    					<td colspan="2" style="padding: 15px 0;">
-    						<input type="checkbox" required> Accept the Terms of Service of the DLZ Website. <a href="#">Terms & Privacy</a>.
-    					</td>
-    				</tr>
-
-    				<tr>
-    					<td>
-    					</td>
-    					<td colspan="2">
-    						<div class="clearfix">
-      							<button type="submit" class="signupbtn">Sign Up</button>
-      							<button type="reset"  class="cancelbtn">Cancellation</button>
-    						</div>
-    					</td>
-    				</tr>
-    				
-  				</table>
-			</form>
+				<div class="clearfix">
+					<input type="text" placeholder="Email or User name" name="username" required>
+					<br>
+					<input type="password" placeholder="Password" name="username" required>
+					<br>
+					<a href="#">forgot password?</a>
+					<br>
+					<button type="submit" class="cancelbtn">Login</button>	
+				</div>	
+			</form>	
 		</div>
 
 <!--footer-->
