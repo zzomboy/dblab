5c790b1e4d34d15e114481f3b5b503381addb696
karanpoj <karanpoj@gmail.com>
2017-09-26 20:16:47 +0700

    contact final complete no more fix

--- contact.php
+++ contact.php
@@ -55,21 +55,21 @@
 					<p>E-mail</p>
 					<input type="text" name="uemail" required>
 					<p>Subject</p>
-					<textarea name="usubject" rows="10" style="width: 100%;" required></textarea>
+					<textarea name="usubject" rows="10" required></textarea>
 					<button class="submit_bt" type="submit">Submit</button>
 				</form>
 			</div>
 			<div class="contact_text_box">
 				<h3>Company Information :</h3>
 				<p>99 Lorem Ipsum Dolor Sit,</p>
-				<p>22-56-2-9 Sit Amet, Lorem,</p>
+				<p style="padding: 0 0 0 0;">22-56-2-9 Sit Amet, Lorem,</p>
 				<p>Thailand</p>
-				<p>Tel.</p>
-				<p>+66-9999-99999</p>
-				<p>+66-8888-88888</p>
+				<p>Tel. 0-9999-99999</p>
+				<p style="padding: 0 0 6px 27px;">0-8888-88888</p>
 				<p>Fax. 02-999-9999</p>
 				<p>Email: info@dlz.com</p>
-				<p>Monday - Saturday 08.30 - 18.10 hrs.</p>
+				<p>Monday - Saturday </p>
+				<p style="padding-top: 0px;">08.30 - 18.10 hrs.</p>
 
 			</div>
 			<div class="clear"></div>
