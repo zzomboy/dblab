e27c9861823dd514c795864bdf3096a2e9442a4e
karanpoj <karanpoj@gmail.com>
2017-11-06 23:05:54 +0700

    update database

--- dblab.sql
+++ dblab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2017 at 08:51 PM
+-- Generation Time: Nov 06, 2017 at 04:59 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -65,19 +65,6 @@ CREATE TABLE `contact` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `payment`
---
-
-CREATE TABLE `payment` (
-  `pay_id` int(11) NOT NULL,
-  `order_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `pay_time` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `product`
 --
 
@@ -103,8 +90,28 @@ INSERT INTO `product` (`pro_id`, `pro_name`, `pro_pic`, `pro_desc`, `pro_price`,
 (1, 'INTEL Core i7-8700K ', 'intel core i7-8700k.png', 'Socket : LGA1151-v2! CPU Core / Thread : 6/12!Frequency : 3.70 GHz!Turbo : 4.70 GHz', 13800, 0, 13800, '3y', 'Brand?INTEL!\r\nModel?Core i7-8700K!\r\nSocket?LGA1151-v2!\r\nCPU Core / Thread?6/12!\r\nFrequency?3.70 GHz!\r\nTurbo?4.70 GHz!\r\nCPU Bus?8 GT/s DMI3!\r\nArchitecture?14nm!\r\nCache L2?12 x 256KB!\r\nCache L3?12MB!\r\nPower Peak?95W!', 1, 1),
 (10, 'INTEL Core i7-7800X', 'INTEL Core i7-7800X .png', 'CPU Core / Thread 6/12!Frequency 3.5 GHz!Turbo 4.00 GHz!', 13700, 10, 12330, '3y', 'Brand?INTEL!Model?Core i7-7800X!Socket?LGA2066!CPU Core / Thread?6/12!Frequency?3.5 GHz!Turbo?4.00 GHz!CPU Bus?8 GT/s DMI3!Architecture?14nm!Cache L2?6 x 256KB!Cache L3?8.25MB!Power Peak?140W!', 0, 1),
 (23, 'a', 'xxx', '', 100, 20, 80, '5y', '', 1, 3),
-(24, 'b', 'xxx', '', 1000, 0, 1000, '3y', '', 1, 7),
-(25, 'c', 'xxx', '', 500, 0, 500, '1y', '', 1, 5);
+(26, 'c', 'xxx', '', 500, 0, 500, '3y', '', 1, 5),
+(27, 'b', 'xxx', '', 100, 10, 90, '5y', '', 1, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL,
+  `status_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`status_id`, `status_name`) VALUES
+(1, 'order receive'),
+(2, 'prepare & package'),
+(3, 'delivered');
 
 -- --------------------------------------------------------
 
@@ -132,7 +139,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_title`, `user_name`, `user_tel`, `user_email`, `user_pw`, `user_gender`, `user_birth`, `user_recip`, `user_rtel`, `user_addr`, `user_type`) VALUES
-(1, 'Mr.', 'Karanpoj Varintaravet', '099999999', 'karanpoj@gmail.com', 'cGFzc3dvcmQ=', 'male', '1996-07-14', 'Karanpoj Varintaravet', '0888888888', '99 Soi Klong Luang 17, Tambon Khlong Nung, Amphoe Khlong Luang, Chang Wat Pathum Thani 12120', 'admin');
+(1, 'Mr.', 'Karanpoj Varintaravet', '099999999', 'karanpoj@gmail.com', 'cGFzc3dvcmQ=', 'male', '1996-07-14', 'Karanpoj Varintaravet', '0888888888', '99 Soi Klong Luang 17, Tambon Khlong Nung, Amphoe Khlong Luang, Chang Wat Pathum Thani 12120', 'admin'),
+(2, 'Mrs.', 'Test1', '0999999999', 'test@email.com', 'cGFzc3dvcmQ=', 'female', '1998-08-04', 'Test1', '0999999999', 'aaaaaaaaaaa', 'member'),
+(4, 'Miss', 'Test2', '0888888888', 'test2@email.com', 'cGFzc3dvcmQ=', 'female', '1992-07-03', 'Test2', '0888888888', 'asdf', 'member');
 
 -- --------------------------------------------------------
 
@@ -144,12 +153,20 @@ CREATE TABLE `user_order` (
   `order_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `order_pros` text NOT NULL,
-  `order_date` date DEFAULT NULL,
+  `order_datetime` datetime DEFAULT NULL,
   `order_to` text,
   `order_cost` float NOT NULL,
-  `order_pay` int(11) NOT NULL,
-  `order_send` int(11) NOT NULL
+  `status_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_order`
+--
+
+INSERT INTO `user_order` (`order_id`, `user_id`, `order_pros`, `order_datetime`, `order_to`, `order_cost`, `status_id`) VALUES
+(1, 1, 'b?90?2?180@INTEL Core i7-7800X?12330?1?12330@a?80?5?400@', '2017-11-05 11:16:29', 'Karanpoj Varintaravet^0888888888^99 Soi Klong Luang 17, Tambon Khlong Nung, Amphoe Khlong Luang, Chang Wat Pathum Thani 12120', 12910, 3),
+(2, 1, 'INTEL Core i7-8700K ?13800?1?13800@c?500?4?2000@', '2017-11-06 15:38:51', 'Karanpoj Varintaravet^0888888888^99 Soi Klong Luang 17, Tambon Khlong Nung, Amphoe Khlong Luang, Chang Wat Pathum Thani 12120', 15800, 1),
+(5, 4, 'INTEL Core i7-7800X?12330?1?12330@', '2017-11-06 21:00:27', 'Test2^0888888888^asdf', 12330, 1);
 
 --
 -- Indexes for dumped tables
@@ -168,17 +185,16 @@ ALTER TABLE `contact`
   ADD PRIMARY KEY (`con_id`);
 
 --
--- Indexes for table `payment`
---
-ALTER TABLE `payment`
-  ADD PRIMARY KEY (`pay_id`),
-  ADD KEY `FK` (`order_id`,`user_id`);
-
---
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`pro_id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
 
 --
 -- Indexes for table `user`
@@ -210,12 +226,22 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `pro_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `pro_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `user_order`
+--
+ALTER TABLE `user_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
