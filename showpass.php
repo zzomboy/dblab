c9301757af91e137149f75511a8ff7fcd90e9b70
karanpoj <karanpoj@gmail.com>
2017-11-21 23:07:42 +0700

    change forgot password

--- showpass.php
+++ showpass.php
@@ -21,18 +21,37 @@
 	}
 	$layout_header->set('title','Forgot Password : IT Online Shopping website');
 	echo $layout_header->output();
+
+	$uemail	= $_POST['uemail'];
+	$utel	= $_POST['utel'];
+	$q 	= "select * from user where user_email='".$uemail."' and user_tel='".$utel."'";
+	$result	= $mysqli->query($q);
+	if(!$result){
+		echo "Error on : $q";
+	}
+
+	$numR = $result->num_rows;
+	if($numR==0)
+	{
+		echo "<script>alert('!! Incorrect Email  or Mobile phone !!');history.back();</script>";
+		exit();
+	}
+	else
+	{
+		$row = $result->fetch_array();
+		$upass = base64_decode($row['user_pw']);
+	}
 ?>
 <!--Content-->
 <!--login form-->
 		<div class="register_form login_form">
-			<h2>Enter your email</h2>
-			<form method="post" action="showpass.php">
+			<h2>Your password is</h2>
+			<form method="post" action="login.php">
 				<div class="clearfix">
-					<input type="text" placeholder="Enter your Email" name="uemail" required>
-					<input type="text" placeholder="Enter your Mobile phone" name="utel" required>
+					<p style="text-align: center;font-size: 17px;"><?php echo $upass ?></p>
 					<br>
-					<button type="submit" class="cancelbtn">Submit</button>	
-				</div>	
+					<button type="submit" class="cancelbtn">Go back to login page</button>
+				</div>
 			</form>	
 		</div>
 <?php
